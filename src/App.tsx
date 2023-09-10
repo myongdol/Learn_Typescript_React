22758333327cf38b6cff93dde9bbfda6ce7d0d55
yong seock <myongdol132@gmail.com>
2023-09-10 23:46:30 +0900

    cross-component comunication

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,13 @@ import NewTodo from './components/NewTodo';
 
 const App:React.FC = () => {
   const todos = [{id: 't1', text: '안녕하세요.'}]
+  const todoAddHandler = (text: string) => {
+    console.log(text);
+  };
+
   return (
     <div className="App">
-      <NewTodo />
+      <NewTodo onAddTodo={todoAddHandler}/>
       <TodoList items={todos}/>
     </div>
   );
